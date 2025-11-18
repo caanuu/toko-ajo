bedb0a907b6cd1d73bea5cefdcff628dc149fb66
caanuu <ihsanarifab12@gmail.com>
2025-11-18 20:14:01 +0700

    first commit

---
+++ database/migrations/2025_11_18_131032_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('sku', 50)->unique();
+            $table->string('name', 30);
+            $table->decimal('sell_price', 15, 2);
+            $table->decimal('stock', 15, 3)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
